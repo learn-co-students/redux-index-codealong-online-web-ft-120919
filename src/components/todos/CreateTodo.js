43a6aaea63a806cdfc9a79f0014bec5d9e7e39d8
jokale <kalemajoanna@gmail.com>
2020-05-15 12:48:55 +0100

    Done.

--- src/components/todos/CreateTodo.js
+++ src/components/todos/CreateTodo.js
@@ -13,7 +13,12 @@ class CreateTodo extends Component {
   handleSubmit = event => {
     event.preventDefault();
     this.props.addTodo(this.state)
+    this.setState({
+      text: '',
+    })
   }
+   
+  
 
   handleChange(event) {
     this.setState({
